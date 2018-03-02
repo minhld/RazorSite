01f713b3d397cb8fe845af52f6e6fa11621a2d9f
minhld <ledinhminh3883@gmail.com>
2018-03-01 23:34:20 -0700

    added new test site

---
+++ ASPSite/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASPSite.Startup))]
+namespace ASPSite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
