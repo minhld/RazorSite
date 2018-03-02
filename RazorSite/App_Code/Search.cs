8275acddf309081055723d948e74b40b849a2f64
minhld <ledinhminh3883@gmail.com>
2018-03-01 17:01:41 -0700

    added some files

---
+++ RazorSite/App_Code/Search.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+/// <summary>
+/// Summary description for Search
+/// </summary>
+public class Search
+{
+    public Search()
+    {
+        // TODO: something
+    }
+    
+    public static string search(string keywords)
+    {
+        List<Employee> emps = new List<Employee>();
+
+        // search in database
+
+        return keywords;
+    }
+}
