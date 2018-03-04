d8444cf016c5afebd71d6ae1d386e2322f7f6fd4
minhld <ledinhminh3883@gmail.com>
2018-03-03 21:29:21 -0700

    adding a new asp mvc project

---
+++ MySite/Controllers/EmployeeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+using MySite.Models;
+
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MySite.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private EmployeeDBContext db = new EmployeeDBContext();
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var emps = from e in db.Employees orderby e.ID select e;
+            return View(emps);
+        }
+
+        // GET: Employee/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee e)
+        {
+            try
+            {
+                db.Employees.Add(e);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/Edit/ID
+        public IActionResult Edit(int ID) 
+        {
+            // var emp = from e in db.Employees select e.ID == ID;
+            var emp = db.Employees.Single(m => m.ID == ID);
+            return View(emp);
+        }
+
+        
+    }
+}
---
+++ MySite/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+
+
+namespace MySite.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime JoinDate { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class EmployeeDBContext : DbContext
+    {
+        public EmployeeDBContext()
+        { }
+        public DbSet<Employee> Employees { get; set; }
+    }
+
+}
