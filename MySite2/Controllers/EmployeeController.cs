d9345fada7473086d9364d62a4e091b38652c8ca
minhld <ledinhminh3883@gmail.com>
2018-03-05 00:33:44 -0700

    added search page for test

--- MySite2/Controllers/EmployeeController.cs
+++ MySite2/Controllers/EmployeeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Script.Serialization;
 
 using MySite2.Models;
 
@@ -15,9 +16,77 @@ namespace MySite2.Controllers
         // GET: Employee
         public ActionResult Index()
         {
-            var empList = db.Employees.ToList();
+            var empList = (from e in db.Employees
+                          join c in db.Companies on e.Company equals c.Id
+                          select new { Name = e.Name,
+                                       Age = e.Age,
+                                       Email = e.Email,
+                                       Company = c.Name,
+                                       Address = e.Address }).ToList();
 
             return View(empList);
+        }
+
+        public ActionResult Search()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string SubmitSearch(string keys)
+        {
+            var empList = (from e in db.Employees
+                           join c in db.Companies on e.Company equals c.Id
+                           where e.Name.Contains(keys)
+                           select new
+                           {
+                               Name = e.Name,
+                               Age = e.Age,
+                               Email = e.Email,
+                               Company = c.Name,
+                               Address = e.Address
+                           }).ToList();
+
+            JavaScriptSerializer serial = new JavaScriptSerializer();
+            return serial.Serialize(empList);
+        }
+
+        [HttpPost]
+        public string SubmitSearchByAge(int keys)
+        {
+            var empList = (from e in db.Employees
+                           join c in db.Companies on e.Company equals c.Id
+                           where e.Age >= keys
+                           select new
+                           {
+                               Name = e.Name,
+                               Age = e.Age,
+                               Email = e.Email,
+                               Company = c.Name,
+                               Address = e.Address
+                           }).ToList();
+
+            JavaScriptSerializer serial = new JavaScriptSerializer();
+            return serial.Serialize(empList);
+        }
+
+        [HttpPost]
+        public string SubmitSearchByCompany(string keys)
+        {
+            var empList = (from e in db.Employees
+                           join c in db.Companies on e.Company equals c.Id
+                           where c.Name.Contains(keys)
+                           select new
+                           {
+                               Name = e.Name,
+                               Age = e.Age,
+                               Email = e.Email,
+                               Company = c.Name,
+                               Address = e.Address
+                           }).ToList();
+
+            JavaScriptSerializer serial = new JavaScriptSerializer();
+            return serial.Serialize(empList);
         }
     }
 }
